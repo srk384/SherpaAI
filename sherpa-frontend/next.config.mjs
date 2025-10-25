a96f222f3cee452bbc92e86f940cdf07d08c552f
srk384 <khanshahrukh384@gmail.com>
2025-10-26 00:07:05 +0530

    add Netlify configuration and update dependencies

--- sherpa-frontend/next.config.mjs
+++ sherpa-frontend/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Add your configuration options here
+  // For example: `output: 'export'`, if needed for static exports.
+
+  async redirects() {
+    return [
+      {
+        source: '/old-path',
+        destination: '/new-path',
+        permanent: true,
+      },
+      // You can add more redirect objects here
+    ];
+  },
 };
 
 export default nextConfig;
